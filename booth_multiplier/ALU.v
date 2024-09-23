665d9d38e3acb94346ad9712098b33b7584b3533
Prerak Gupta <118615112+prerak2323@users.noreply.github.com>
2024-09-23 23:28:47 +0530

    Add files via upload

---
+++ booth_multiplier/ALU.v
@@ -0,0 +1,15 @@
+module ALU (out,in1,in2,addsub);
+
+  input addsub ;  // addsub = 1 then addition else subtraction
+  input signed[15:0] in1,in2;
+  output reg signed[15:0] out;
+  
+  always@(*)
+    begin
+      if(addsub)
+	      out  = in1 + in2;
+      else if(!addsub)
+	      out  = in1 - in2;
+	end
+	
+endmodule
